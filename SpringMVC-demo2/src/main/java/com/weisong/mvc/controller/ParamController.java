4be8249d72061bc7a38d88c0044659d188fc1a50
liweisong-java <liweisong0628@gmail.com>
2021-10-28 12:12:35 +0800

    回顾原生的Servlet获取请求参数

---
+++ SpringMVC-demo2/src/main/java/com/weisong/mvc/controller/ParamController.java
@@ -0,0 +1,9 @@
+package com.weisong.mvc.controller;
+
+/**
+ * @author 李伟松
+ * @create 2021-10-28-12:07
+ */
+public class ParamController {
+
+}
--- SpringMVC-demo2/src/main/java/com/weisong/mvc/controller/TestController.java
+++ SpringMVC-demo2/src/main/java/com/weisong/mvc/controller/TestController.java
@@ -20,4 +20,10 @@ public class TestController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping("/param")
+    public String param(){
+        return "test_param";
+    }
+
 }

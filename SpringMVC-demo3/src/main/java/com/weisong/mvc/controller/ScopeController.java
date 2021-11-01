1ff0af089bdd207bc60a0bff6017eb52c4f0bfea
liweisong-java <liweisong0628@gmail.com>
2021-11-01 14:02:50 +0800

    通过ServletAPI向application域对象共享数据

--- SpringMVC-demo3/src/main/java/com/weisong/mvc/controller/ScopeController.java
+++ SpringMVC-demo3/src/main/java/com/weisong/mvc/controller/ScopeController.java
@@ -1,12 +1,14 @@
 package com.weisong.mvc.controller;
 
 import com.sun.net.httpserver.Authenticator;
+import javafx.application.Application;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
@@ -57,7 +59,13 @@ public class ScopeController {
     @RequestMapping("/testSession")
     public String testSession(HttpSession session){
         session.setAttribute("testSessionScope", "hello,session");
-        System.out.println(session);
+        return "success";
+    }
+
+    @RequestMapping("/testApplication")
+    public String testApplication(HttpSession session){
+        ServletContext application = session.getServletContext();
+        application.setAttribute("TestApplicationScope", "hello,application");
         return "success";
     }
 }

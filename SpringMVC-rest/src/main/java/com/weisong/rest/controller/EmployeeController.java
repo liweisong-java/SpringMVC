45449be42d39b64673058127b4739d3aacc3f18c
李伟松 <liweisong0628@gmail.com>
2021-11-04 14:28:38 +0800

    实现列表功能

--- SpringMVC-rest/src/main/java/com/weisong/rest/controller/EmployeeController.java
+++ SpringMVC-rest/src/main/java/com/weisong/rest/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.weisong.rest.controller;
 
+import com.weisong.rest.bean.Employee;
 import com.weisong.rest.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
 
 /**
  * @author 李伟松
@@ -12,4 +18,13 @@ import org.springframework.stereotype.Controller;
 public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getAllEmployee(Model model){
+        //获取员工集合
+        Collection<Employee> employeeList = employeeDao.getAll();
+        //想要在页面上访问需要把employeeList保存到域对象中
+        model.addAttribute("employeeList",employeeList);
+        return "employee_List";
+    }
 }
